efc467e8eeadebb9cfbb3927dc3ac5065ef59116
fcuadra-99 <fcuadra_230000001491@uic.edu.ph>
2025-05-20 12:54:22 +0800

    v.3.1

--- src/assets/Scene1.tsx
+++ src/assets/Scene1.tsx
@@ -14,7 +14,7 @@ const ThreeScene: React.FC = () => {
                     window.innerWidth / window.innerHeight,
                     0.1,
                     1000),
-                rend: new THREE.WebGLRenderer()
+                rend: new THREE.WebGLRenderer(),
             };
 
 
@@ -27,13 +27,15 @@ const ThreeScene: React.FC = () => {
                 jumpHeight: .2,
                 gravity: .01,
                 velocity: 0,
-
+                horsens: 0.2,
                 jumps: false
             };
 
+
             // Ctrl
             const m = { x: 0, y: 0 };
-            let ctrlon = false;
+            let ctrlon = true;
+            const originalUp = new THREE.Vector3(0, 1, 0);
 
             document.addEventListener('mouseenter', () => ctrlon = true);
             document.addEventListener('mouseleave', () => ctrlon = false);
@@ -51,34 +53,53 @@ const ThreeScene: React.FC = () => {
 
             function ctrl() {
                 const dir = new THREE.Vector3();
-
+                const yaw = new THREE.Vector3(1, 0, 0);
+                const pitch = new THREE.Vector3(0, 1, 0);
                 cam.getWorldDirection(dir).normalize();
                 dir.y = 0;
+
 
                 if (controls[87]) {
                     if (controls[16]) {
                         cam.position.addScaledVector(dir, player.speed * 2);
+                    } else {
+                        cam.position.addScaledVector(dir, player.speed);
                     }
-                    cam.position.addScaledVector(dir, player.speed);
                 }
                 if (controls[83]) cam.position.addScaledVector(dir, -player.speed);
                 const side = new THREE.Vector3().crossVectors(dir, cam.up).normalize();
                 if (controls[65]) cam.position.addScaledVector(side, -player.speed);
                 if (controls[68]) cam.position.addScaledVector(side, player.speed);
+                if (0.6 < m.y / window.innerWidth && ctrlon) {
+                    if (cam.rotation.x < -0.5) return;
+                    const mult = m.y / (window.innerHeight * 0.4);
+                    cam.rotateOnAxis(yaw, -0.002 * (1.5 * Math.exp(mult)));
+                    cam.up.copy(originalUp);
+                }
+                if (0.4 > m.y / window.innerWidth && ctrlon) {
+                    if (cam.rotation.x > 0.8) return;
+                    const mult = (window.innerHeight - m.y) / (window.innerHeight * 0.4);
+                    cam.rotateOnAxis(yaw, 0.002 * (1.5 * Math.exp(mult)));
+                    cam.up.copy(originalUp);
+                }
                 if (0.7 < m.x / window.innerWidth && ctrlon) {
                     const mult = m.x / (window.innerWidth * 0.3);
-                    cam.rotation.y -= player.turnSpeed * (1.5 * Math.exp(mult));
+                    cam.rotateOnAxis(pitch, -0.001 * (1.5 * Math.exp(mult)));
+                    cam.up.copy(originalUp);
+                    cam.lookAt(cam.position.clone().add(cam.getWorldDirection(new THREE.Vector3())));
                 }
                 if (0.3 > m.x / window.innerWidth && ctrlon) {
                     const mult = (window.innerWidth - m.x) / (window.innerWidth * 0.3);
-                    cam.rotation.y += player.turnSpeed * (1.5 * Math.exp(mult));
+                    cam.rotateOnAxis(pitch, 0.001 * (1.5 * Math.exp(mult)));
+                    cam.up.copy(originalUp);
+                    cam.lookAt(cam.position.clone().add(cam.getWorldDirection(new THREE.Vector3())));
                 }
+
                 if (controls[32]) {
                     if (player.jumps) return false;
                     player.jumps = true;
                     player.velocity = -player.jumpHeight;
                 }
-
             }
 
             // Objs
@@ -106,6 +127,17 @@ const ThreeScene: React.FC = () => {
                             opacity: 0.2
                         }),
                         pos: new THREE.Vector3(0, 2, 0)
+                    },
+                    {
+                        name: 'plyr',
+                        geo: new THREE.BoxGeometry(0.1, 0.1, 0.1),
+                        mat: new THREE.MeshBasicMaterial({
+                            color: 'skyblue',
+                            wireframe: true,
+                            transparent: true,
+                            opacity: 0.2
+                        }),
+                        pos: new THREE.Vector3(0, -0.2, 0)
                     }
                 ];
 
@@ -129,6 +161,7 @@ const ThreeScene: React.FC = () => {
             }
 
             function init() {
+                scen.fog = new THREE.Fog(0xcccccc, 1, 15);
                 cam.position.z = 5;
                 cam.rotation.z = 0;
                 rend.setSize(window.innerWidth, window.innerHeight);
@@ -144,6 +177,7 @@ const ThreeScene: React.FC = () => {
             const renderScene = () => {
                 ctrl();
                 ixup();
+                mesh.plyr.position.copy(cam.position);
                 mesh.cube.rotation.x += 0.01;
                 mesh.cube.rotation.y += 0.01;
                 rend.render(scen, cam);
