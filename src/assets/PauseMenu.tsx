cb7318c5a3e82e1bf9a94b0d909c19d5f74a7e09
fcuadra-99 <fcuadra_230000001491@uic.edu.ph>
2025-05-23 16:22:03 +0800

    v3.5.3

--- src/assets/PauseMenu.tsx
+++ src/assets/PauseMenu.tsx
@@ -2,15 +2,58 @@ import { useState } from 'react';
 
 let val = 0.002;
 
-function tPause() {
-    document.body.requestPointerLock({ unadjustedMovement: true });
-}
-
 function PauseMenu(ctrlon: boolean, s: number) {
     const [value, setValue] = useState(val);
+    const [opt, togopt]= useState(true);
     console.log('s', s)
 
+    return (
+        <>
+            <div onClick={tPause} className={`pbg ${ctrlon ? "active" : "inactive"}`} />
+            <section className={`pmenu ${ctrlon ? "active" : "inactive"}`}>
+                <h1 className='labe'>Paused</h1>
+                <p className='labe'>Click to Continue</p>
+                <Btns />
+                {Slider()}
+            </section>
+        </>
+    )
+
+    function ShowOption() {
+        togopt(!opt);
+        console.log(opt);
+    }
+
+    function Btns() {
+        return (
+            <>
+                <div className='btns'>
+                    <div className='exit'></div>
+                    <div className='opti' onClick={ShowOption}></div>
+                    <div className='info'></div>
+                </div>
+            </>
+        )
+    }
+
     function Slider() {
+        const style = {
+            slider: {
+                position: 'fixed',
+                display: 'flex',
+                top: '0',
+                right: '0',
+                margin: '30px',
+                padding: '0px 20px',
+                justifyContent: 'space-between'
+            } as React.CSSProperties,
+
+            label: {
+                fontSize: 'medium',
+                alignSelf: 'flex-end',
+                marginRight: '10px'
+            } as React.CSSProperties
+        }
         const handleInputChange = (event: { target: { value: any; }; }) => {
             setValue(Number(event.target.value));
             val = value;
@@ -18,28 +61,25 @@ function PauseMenu(ctrlon: boolean, s: number) {
         };
 
         return (<>
-            <h1>{`Sensitivity: ${(value * 1000).toFixed(2)}`}</h1>
-            <input type="range"
-                min={0.00001} max={0.005}
-                step={0.00001}
-                value={value}
-                onChange={handleInputChange}
-                className="slider"
-            />
+            <div className={`slaida ${opt ? "active" : "inactive"}`} style={style.slider}>
+                <h1 style={style.label}>
+                    {`Sensitivity: ${(value * 1000).toFixed(2)}`}
+                </h1>
+                <input type="range"
+                    min={0.00001} max={0.005}
+                    step={0.00001}
+                    value={value}
+                    onChange={handleInputChange}
+                    className="slider"
+                />
+            </div>
+
         </>)
     }
+}
 
-    return (
-        <>
-            <div onClick={tPause} className={`pbg ${ctrlon ? "active" : "inactive"}`} />
-            <section className={`pmenu ${ctrlon ? "active" : "inactive"}`}>
-                <h2 className='labe'>Paused</h2>
-                <div className='slaida'>
-                    {Slider()}
-                </div>
-            </section>
-        </>
-    )
+function tPause() {
+    document.body.requestPointerLock({ unadjustedMovement: true });
 }
 
 export function sens() {
